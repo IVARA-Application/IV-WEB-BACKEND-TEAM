bd8b0bccf1390fd78345f8216e38ed417b49990b
Teghdeep Kapoor <teghdeep@gmail.com>
2021-02-07 17:26:18 +0530

    Google OAuth Progress-1

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const authController = require('../controllers/auth');
+const cors = require("./cors");
 
 const router = express.Router();
-
-router.post('/google', authController.login);
+router.options(cors.corsWithOptions, (req, res) => { res.sendStatus(200) })
+router.post('google',cors.corsWithOptions, authController.login);
 
 module.exports = router;
